58957a4dc5af0f9601fcd67a55fea0d5feef342b
Max Baun <max@d3applications.com>
2018-02-21 00:10:59 -0500

    removing unused packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,7 @@
 module.exports = {
 	plugins: [
-		// require('stylelint'),
 		require('postcss-import'),
-		// require('postcss-automath'),
 		require('precss'),
-		// require('postcss-color-function'),
-		// require('postcss-mixins'),
-		require('postcss-utilities'),
-		// require('postcss-simple-vars'),
-		require('postcss-nested'),
-		require('postcss-nesting'),
 		require('postcss-cssnext')
 	]
 };
