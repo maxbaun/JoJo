b3d1130e1e6bb7caa239ff12db8754969f2bd61a
Max Baun <max@d3applications.com>
2018-02-23 11:42:28 -0500

    adding stories/contentful integration

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -1,56 +1,72 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import graphql from 'graphql';
+import axios from 'axios';
 import {fromJS, List} from 'immutable';
 import Parser from 'html-react-parser';
+import Showdown from 'showdown';
 
 import CSS from '../css/pages/stories.module.css';
 import Page from '../components/page';
 
-const Stories = ({data: d}) => {
-	const data = fromJS(d.allStoryJson);
-
-	const stories = data.getIn(['edges']).map(e => e.get('node'));
-
-	const PageConfig = {
-		title: `JoJo Stories`,
-		description: `Stories of JoJo, a dolphin from Providenciales in the Turks & Caicos Islands.`,
-		keywords: List(['encounters', 'stories'])
-	};
-
-	return (
-		<Page {...PageConfig}>
-			<div className={CSS.stories}>
-				{stories.map(story => {
-					return (
-						<div key={story.get('title')} className={CSS.story}>
-							<div className={CSS.storyContent}>
-								<h3>{story.get('title')}</h3>
-								{Parser(story.get('content') || '')}
-							</div>
-						</div>
-					);
-				})}
-			</div>
-		</Page>
-	);
+const PageConfig = {
+	title: `JoJo Stories`,
+	description: `Stories of JoJo, a dolphin from Providenciales in the Turks & Caicos Islands.`,
+	keywords: List(['encounters', 'stories'])
 };
 
-Stories.propTypes = {
-	data: PropTypes.object.isRequired
-};
+export default class Stories extends React.Component {
+	constructor(props) {
+		super(props);
 
-export const pageQuery = graphql`
-	query Stories {
-		allStoryJson(sort: {fields: [order], order: DESC}) {
-			edges {
-				node {
-					title
-					content
-				}
-			}
-		}
+		this.converter = new Showdown.Converter();
+
+		this.state = {
+			stories: List()
+		};
 	}
-`;
 
-export default Stories;
+	componentDidMount() {
+		this.getStories();
+	}
+
+	getStories() {
+		const config = {
+			url: `https://cdn.contentful.com/spaces/smkpl8jy8b0t/entries`,
+			params: {
+				content_type: 'story', //eslint-disable-line
+				access_token: '9a4ceb123334195b06c64d2a037cb9afd3bb198a473d5f490d2ed59c3c4c1a65', //eslint-disable-line
+				order: '-fields.order'
+			}
+		};
+
+		return axios(config)
+			.then(r => r.data)
+			.then(data => {
+				this.setState({
+					stories: fromJS(data.items)
+				});
+			});
+	}
+
+	render() {
+		const {stories} = this.state;
+
+		return (
+			<Page {...PageConfig}>
+				<div className={CSS.stories}>
+					<p className={CSS.info}>This is a group of stories that have been told to me about JoJo. They all happened before I met him. I don’t know if any of them are true I suspect some are and some aren’t! If any of you out there have more to add please let me know and I will include them.</p>
+					{stories.map(story => {
+						console.log(Parser(story.getIn(['fields', 'content'])));
+						return (
+							<div key={story.getIn(['sys', 'id'])} className={CSS.story}>
+								<div className={CSS.storyContent}>
+									<h3>{story.getIn(['fields', 'title'])}</h3>
+									{Parser(this.converter.makeHtml(story.getIn(['fields', 'content'])))}
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</Page>
+		);
+	}
+}
